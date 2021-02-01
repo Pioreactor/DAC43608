5dc43781740ff8aa7e18e6f9c5748efa215b531c
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-02-01 14:49:30 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DAC43608',
+      version='0.1',
+      description='A python wrapper for interacting with the DAC43608',
+      author='Cam Davidson-Pilon',
+      author_email='cam@pioreactor.com',
+      url='https://github.com/Pioreactor/DAC43608',
+      packages=['DAC43608'],
+      license="MIT"
+     )
